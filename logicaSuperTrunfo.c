c12927a7031a4ce7280b92f1599fd82af7a1e86a
Camila Ribeiro <camilaribeirogloria@gmail.com>
2025-06-15 23:03:18 +0000

    atualizacao menu switch

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
+// Tema 2 - Comparação das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
